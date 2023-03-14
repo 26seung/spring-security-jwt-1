50a452c0d3d22e71ecb8487e2ac1e8e05b5c7f09
Euseung <yousi7@naver.com>
2023-03-14 23:34:56 +0900

    refreshrepo

--- src/main/java/com/backend/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/backend/jwt/repository/RefreshTokenRepository.java
@@ -1,2 +1,15 @@
-package com.backend.jwt.repository;public interface RefreshTokenRepository {
+package com.backend.jwt.repository;
+
+import com.backend.jwt.domain.RefreshToken;
+import com.backend.jwt.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+//    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+    Optional<RefreshToken> findByUuid(String uuid);
+
 }
