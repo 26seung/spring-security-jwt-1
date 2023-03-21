ae30e130c9774fa515e8d29db54d7207109abf40
Euseung <yousi7@naver.com>
2023-03-21 23:56:27 +0900

    redis

--- src/main/java/com/backend/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/backend/jwt/repository/RefreshTokenRepository.java
@@ -1,15 +1,13 @@
 package com.backend.jwt.repository;
 
 import com.backend.jwt.domain.RefreshToken;
-import com.backend.jwt.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
+//  repository 의 save 사용시 hash 형태로 값이 저장된다.
+//  hash 형태의 값을 확인하기 위해서는 hkeys : 저장된 key 값, hvals : 저장된 value 값, hgetall : 저장된 key, value 값
+//  ex : hvals refresh_token:test ,  hgetall refresh_token:test
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
-//    Optional<RefreshToken> findByRefreshToken(String refreshToken);
-    Optional<RefreshToken> findByUuid(String uuid);
 
+    RefreshToken findByRefreshTokenId(String username);
 }
